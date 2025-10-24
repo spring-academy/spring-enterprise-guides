3c3420328aa5c484356dc16a5e429253d5be0cf4
Ryan Schneider <djschny@users.noreply.github.com>
2025-10-23 20:39:06 -0400

    add advisor binary installation

--- labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
+++ labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
@@ -4,3 +4,6 @@ set -x
 set -eo pipefail
 
 (cd /opt/git/repositories && git clone --bare https://github.com/timosalm/spring-petclinic-2.7 spring-petclinic && echo ".advisor" >> .gitignore)
+
+export PATH=${PATH}:/opt/tanzu/bin
+echo "export PATH=${PATH}:${HOME}/opt/tanzu/bin" >> ${HOME}/.bash_profile
