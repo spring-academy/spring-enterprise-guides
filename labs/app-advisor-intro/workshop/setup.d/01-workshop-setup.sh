d34d965a1b6c68b06c7343c5684131673d0ce4c1
Timo Salm <accounts@timosalm.de>
2026-03-09 17:42:38 +0100

    Fix workshop init script

--- labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
+++ labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
@@ -7,7 +7,7 @@ curl -s "https://get.sdkman.io" | bash && \
 echo "sdkman_auto_answer=true" > $HOME/.sdkman/etc/config && \
 echo "sdkman_auto_selfupdate=false" >> $HOME/.sdkman/etc/config && \
 source "$HOME/.sdkman/bin/sdkman-init.sh" && \
-sdk install java $(sdk list java | grep  "17.*[0-9]-librca" | awk '{print $NF}' | head -n 1) && \
+sdk install java $(sdk list java | grep  "17.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
 sdk install java $(sdk list java | grep  "11.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
 
 (cd /opt/git/repositories && git clone --bare https://github.com/timosalm/spring-petclinic-2.7 spring-petclinic && echo ".advisor" >> .gitignore)
