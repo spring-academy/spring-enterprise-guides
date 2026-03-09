aa116ae3c4e3ce5c17e161ed02cb907517f7b72f
Timo Salm <accounts@timosalm.de>
2026-03-09 17:48:17 +0100

    Fix workshop init script

--- labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
+++ labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 curl -s "https://get.sdkman.io" | bash && \
 echo "sdkman_auto_answer=true" > $HOME/.sdkman/etc/config && \
 echo "sdkman_auto_selfupdate=false" >> $HOME/.sdkman/etc/config && \
-source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java $(sdk list java | grep  "17.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
 sdk install java $(sdk list java | grep  "11.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
 
