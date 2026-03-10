1092ee12aab21f86d2459a5bb61695390d872885
Timo Salm <accounts@timosalm.de>
2026-03-10 12:40:29 +0100

    Fix workshop init script

--- labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
+++ labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 set -x
-set -eo pipefail
+set -e
 
-curl -s "https://get.sdkman.io" | bash
+# 1. Install SDKMAN if not present
+if [ ! -d "$HOME/.sdkman" ]; then
+    curl -s "https://get.sdkman.io?rcupdate=false" | bash
+fi
 
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
+# 2. Load SDKMAN into the current shell session
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java 17.0.18-librca
-sdk install java 11.0.30-librca
+# 3. FORCE non-interactive mode for automated installs
+export sdkman_auto_answer=true
+
+# 4. Install Java versions
+# Note: Using '|| true' helps if the version is already installed
+sdk install java 17.0.18-librca || true
+sdk install java 11.0.30-librca || tru
 
 mkdir -p /opt/git/repositories
 (cd /opt/git/repositories && git clone --bare https://github.com/timosalm/spring-petclinic-2.7 spring-petclinic && echo ".advisor" >> .gitignore)
