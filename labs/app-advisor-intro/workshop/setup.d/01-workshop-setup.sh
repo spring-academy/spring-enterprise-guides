0ad9e8f07c26d8358cdf7410f9e0207cf083ae44
Timo Salm <accounts@timosalm.de>
2026-03-10 12:08:28 +0100

    Fix workshop init script

--- labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
+++ labs/app-advisor-intro/workshop/setup.d/01-workshop-setup.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
-
 set -x
-set -eo pipefail
 
-curl -s "https://get.sdkman.io" | bash && \
-echo "sdkman_auto_answer=true" > $HOME/.sdkman/etc/config && \
-echo "sdkman_auto_selfupdate=false" >> $HOME/.sdkman/etc/config && \
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java $(sdk list java | grep  "17.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
-sdk install java $(sdk list java | grep  "11.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
+set -o pipefail
 
+curl -s "https://get.sdkman.io" | bash
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+echo "sdkman_auto_answer=true" > "$SDKMAN_DIR/etc/config"
+echo "sdkman_auto_selfupdate=false" >> "$SDKMAN_DIR/etc/config"
+
+set -e
+
+JAVA_17=$(sdk list java | grep "17.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
+sdk install java "$JAVA_17"
+
+JAVA_11=$(sdk list java | grep "11.*[0-9]-librca" | awk '{print $NF}' | head -n 1)
+sdk install java "$JAVA_11"
+
+mkdir -p /opt/git/repositories
 (cd /opt/git/repositories && git clone --bare https://github.com/timosalm/spring-petclinic-2.7 spring-petclinic && echo ".advisor" >> .gitignore)
