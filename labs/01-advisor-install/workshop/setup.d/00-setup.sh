f6a9afbc153bcea97ff565faef39cfb0fb4fb6aa
Chris DeLashmutt <chris.delashmutt@broadcom.com>
2026-03-23 15:18:16 -0400

    Update deps

--- labs/01-advisor-install/workshop/setup.d/00-setup.sh
+++ labs/01-advisor-install/workshop/setup.d/00-setup.sh
@@ -11,3 +11,4 @@ rm /home/eduk8s/hello-spring-boot-1-5.tgz
 # Extract dependencies for test app
 echo "Extracting ~/.m2/repository directory"
 tar -xzf /home/eduk8s/deps.tgz -C $HOME/ 
+rm /home/eduk8s/deps.tgz
